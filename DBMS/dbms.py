f0fc23eed00304e3b91cd4269cbe68a058493e22
MUKESH <119560022+Mukesh07-R@users.noreply.github.com>
2023-12-01 11:20:25 +0530

    Update dbms.py

--- DBMS/dbms.py
+++ DBMS/dbms.py
@@ -7,8 +7,8 @@ conn = mysql.connector.connect(
     host="127.0.0.1",
     port=3306,
     user="root",
-    password="Mukesh77",
-    database="onlinevotingdb"
+    password="Password",
+    database="Database name"
 )
 try:
     st.session_state.logged_in
